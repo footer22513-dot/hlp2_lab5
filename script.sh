04b302d90ff9190b9cff963ab65b928ebe071ab0
footer22513-dot <footer22513@gmail.com>
2026-06-01 00:44:36 +0300

    Remove code comments

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# g++ -std=c++17 main.cpp auth.cpp func_module/func.cpp menu/CMenu.cpp menu/CMenuItem.cpp models/user/user.cpp models/product/product.cpp models/supplier/supplier.cpp models/worker/worker.cpp models/employee/employee.cpp my_vec/vec.cpp my_vec/account_vec.cpp -o app
-
-# Компилируем проект
 g++ -std=c++17 -I include \
   src/main.cpp \
   src/auth.cpp \
